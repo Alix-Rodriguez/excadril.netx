a5fbefcafc8314ffd1f25a315cd80ffd919c1e6d
Alix Rodriguez <alix.rodriguez@jirehpro.co>
2023-08-09 19:41:16 -0400

    conexion de back y frond

--- components/layouts/FormularioContacto.tsx
+++ components/layouts/FormularioContacto.tsx
@@ -1,18 +1,52 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useForm } from "react-hook-form";
 
 
 import { ScriptProps } from 'next/script';
+
+
 import { gradianteYellow } from '../styles';
+import axios from 'axios';
 
 
 
 
-
-
-
+const submit = async (date:any)=>{
+  const form = {
+    data:{}
+  }
+  form.data = date
+  console.log(form)
+  try {
+    const response = await axios.post('http://marcelina.store/alix/api/formularios',form);
+    // res.status(200).json(response.data);
+    console.log(response)
+  } catch (error) {
+    // res.status(500).json({ message: 'Error al realizar la solicitud POST' });
+    console.error("FALLO EN ENVIO DEL FORMULARIO")
+  }
+}
 
 export const FormularioContacto: FC = () => {
+  
+  const {register, handleSubmit } = useForm()
 
+  // useEffect(() => {
+    
+  //   const apiHandler = async () => {
+  //    console.log("entroooo")
+  //    try {
+  //      const response = await axios.get('http://localhost:1337/api/pruebas');
+  //      const data = response.data;
+  //     console.log(data)
+  //    } catch (error) {
+  //      console.error(error);
+  //    }
+  // };
+
+  // apiHandler();
+
+  // }, []);
   
 
     return (
@@ -20,13 +54,13 @@ export const FormularioContacto: FC = () => {
 
                 <h2 style={{fontFamily:'"Anton", sans-serif'}} className='mb-16 text-5xl'>CONTACTO</h2>
 
-                <form className='p-14' action="">
-                  <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:shadow-outline" id="" type="text" placeholder="Username"/>
+                <form className='p-14' onSubmit={handleSubmit(submit)}>
+                  <input  {...register('nombre')} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:shadow-outline"  type="text" placeholder="Username"/>
                   
-                  <input className="my-6 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:shadow-outline" id="" type="email" placeholder="Email"/>
+                  <input {...register('email')} className="my-6 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:shadow-outline"  type="email" placeholder="Email"/>
                  
 
-                  <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:shadow-outline" placeholder="Mensaje" name="" id="" cols={30} rows={10}></textarea>
+                  <textarea {...register('descripcion')} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:shadow-outline" placeholder="Mensaje"  cols={30} rows={10}></textarea>
 
 
                   <button className='text-lg border rounded w-full py-2 px-3 mt-5 text-white' style={gradianteYellow} type='submit'>ENVIAR</button>
